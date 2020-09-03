48d520fb400c4971e330a9338cbbed91ab7fdda8
gabrielvalerios <gabrieldovalerios@gmail.com>
2020-09-03 19:57:34 -0300

    Version 1.0 of memeGenerator using react

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+class MemeGenerator extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImg: "http://i.imgflip.com/1bij.jpg",
+      memeImgs: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((response) => {
+        const { memes } = response.data;
+        this.setState({
+          memeImgs: memes,
+        });
+      });
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = () => {
+    event.preventDefault();
+
+    const randomNumber = Math.floor(Math.random() * this.state.memeImgs.length);
+    const randomMeme = this.state.memeImgs[randomNumber].url;
+    this.setState({
+      randomImg: randomMeme,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="topText"
+            placeholder="Top Text"
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="Bottom Text"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
+          <button>Generate</button>
+        </form>
+
+        <div>
+          <img src={this.state.randomImg} alt="" />
+          <h2>{this.state.topText}</h2>
+          <h2>{this.state.bottomText}</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MemeGenerator;
